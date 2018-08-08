b55382876af5a608027b3ee7c5ae370f2942a3b6
Maciej Kurek <maciejk77@gmail.com>
2018-08-08 16:41:58 +0100

    some styles, custom colors, grid/list toggle applied

--- src/components/card.js
+++ src/components/card.js
@@ -4,19 +4,20 @@ import '../styles/card.css';
 const Card = (props) => {
   const { shortTitle, doi, publicationType, publicationDate, 
           abstract, nViews, nDownloads, journalId } = props.item;
+  const link = `http://doi.org/${doi}`;
           
   return (
     <div>
-      <div>title: {shortTitle}</div>
-      <div>doi: {doi}</div>
-      <div>type: {publicationType}</div>
-      <div>date: {publicationDate}</div>
+      <div className="dot" style={{backgroundColor: props.color_palette[journalId.toLowerCase()]}}></div>
+      <div>{shortTitle}</div>
+      <div>{link}</div>
+      <div>{publicationType}</div>
+      <div>{publicationDate}</div>
       {props.is_list_view &&   
         <div className="card-abstract" dangerouslySetInnerHTML={{ __html: abstract }}></div>
       }
-      <div>v: {nViews}</div>
-      <div>d: {nDownloads}</div>
-      <div>{journalId}</div>
+      <div>{nViews}</div>
+      <div>{nDownloads}</div>
     </div>
     )  
 }
--- src/components/cards_group.js
+++ src/components/cards_group.js
@@ -4,6 +4,14 @@ import data from '../data/publications.json';
 import '../styles/cards_group.css';
 import cn from 'classnames';
 
+const color_palette = {
+  red: '#f08080', // lightcoral to replace red
+  blue: '#ADD8E6', // lightblue
+  green: '#90EE90', // lightgreen
+  yellow: '#FFFF82', // lighter yellow
+  orange: '#F9D188' // lighter orange
+}
+
 class CardsGroup extends Component {
   constructor(props) {
     super(props)
@@ -18,7 +26,8 @@ class CardsGroup extends Component {
   render_cards() {
     return data.map(prop => {
       return (
-        <div key={prop.id} 
+        <div key={prop.id}
+          style={{borderLeftColor: color_palette[prop.journalId.toLowerCase()], borderLeftWidth: ".25rem"}} 
           className={cn('card', 
             {'card-list-view': this.state.is_list_view}, 
             {'card-grid-view': !this.state.is_list_view})} 
@@ -28,6 +37,7 @@ class CardsGroup extends Component {
           <Card 
             item={prop} 
             is_list_view={this.state.is_list_view}
+            color_palette={color_palette}
           />
         </div>
       )
@@ -53,5 +63,3 @@ class CardsGroup extends Component {
 }
   
 export default CardsGroup;
-
-// className={cn('card', {'card-list-view': prop.is_list_view })}
